f56f4a99c7c745c6110505d9bf85563d02bc52ba
Anaya Mehta <anaya.mehta@gmail.com>
2020-10-24 17:29:01 -0500

    can fetch representatives given an address

--- politx/src/App.js
+++ politx/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [reps, setReps] = useState();
+  const [address, setAddress] = useState();
+
+  const apiKey = "AIzaSyAwjcLpuwIkHOZNdVAkJalXK2fyYSJhBv8";
+
+  const findReps = (userAddress) => {
+    userAddress = encodeURI(userAddress)
+    const url = `https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=${userAddress}&includeOffices=true&key=${apiKey}`;
+    fetch(url)
+    .then(response => {return response.json()})
+    .then(searchResults => {
+      //console.log(searchResults);
+      console.log(searchResults.officials[0].name)
+    })
+    .catch(err => console.log(err))
+  }
+
+  let changeAddress = (event) => {
+    setAddress(event.target.value);
+  }
+
+  //find the representatives based on the value in the address
+  let addressChangeHandler = () =>{
+    findReps(address);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code> Anaya is awesome</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p> Politx! for HackTX 2020 </p>
+      <input
+        id='address'
+        onChange={changeAddress}
+        placeholder="Enter your address to know your representatives" />
+      <button onClick={addressChangeHandler}>Find my representatives!</button>
+      {reps}
     </div>
   );
 }
