152945df9c6f9608003f11bcac0f8ec5e92d2436
Anaya Mehta <anaya.mehta@gmail.com>
2020-10-25 02:32:24 -0500

    can find bills by inputting a name

--- politx/src/App.js
+++ politx/src/App.js
@@ -1,20 +1,23 @@
 
 import './App.css';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 function App() {
-
+  //states
   const [reps, setReps] = useState();
   const [address, setAddress] = useState();
   const [memberID, setMemberID] = useState("");
   const [details, setDetails] = useState();
   const [bills, setBills] = useState();
+  const [name, setName] = useState();
+  const [chamber, setChamber] = useState();
 
   //const [objreps, setObjReps] = useState();
   const objrep = []
   const apiKey = "AIzaSyAwjcLpuwIkHOZNdVAkJalXK2fyYSJhBv8";
   const apiKey2 = "Am0fjAIaHBG95NK3x07FU2GWcouY0HD5pVLsw72z";
 
+  //find a representative based on where you live
   const findReps = (userAddress) => {
     userAddress = encodeURI(userAddress)
     const url = `https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=${userAddress}&includeOffices=true&key=${apiKey}`;
@@ -51,63 +54,6 @@ function App() {
     .catch(err => console.log(err))
   }
 
-  const findBills = (memberID) => {
-    let type = "cosponsored"
-    //let memberID =  // member id for Abdnor James (Republican - South Dakota)ssssss
-    const url = `https://api.propublica.org/congress/v1/members/${memberID}/bills/${type}.json`;
-    fetch(url,{
-      headers: {
-        "X-API-Key": apiKey2
-      }
-    })
-    .then(response => {return response.json()})
-    .then(searchResults => {
-      console.log(searchResults)
-      let results = searchResults.results[0].bills;
-      //array of names of officials in those offices
-      // let officials = searchResults.officials;
-      var billsArray = [];
-
-      if (results == undefined) {
-        billsArray.push("No bills to report.")
-      } else {
-        for(var i = 0; i < results.length; i++) {
-          const billRow = <div key={i}>
-            <h3> Bill- {results[i].short_title}: </h3>
-            <h6> Latest action date: {results[i].latest_major_action_date} </h6>
-            <h6>  Primary Subject: {results[i].primary_subject} </h6>
-            <h6>  Still active?: {results[i].active} </h6>
-            <h6>  Brief: {results[i].summary} </h6>
-            
-          </div>
-          billsArray.push(billRow);
-        }
-      }
-      //setting reps to list of all representatives' repRows
-      setBills(billsArray);
-
-    })
-    .catch(err => console.log(err))
-  }
-
-  let changeAddress = (event) => {
-    setAddress(event.target.value);
-  }
-
-  let changeID = (event) => {
-    setMemberID(event.target.value);
-  }
-
-  //find the representatives based on the value in the address
-  let addressChangeHandler = () =>{
-    findReps(address);
-  }
-
-  let billHandler = () => {
-    // john lewis : L000287
-    findBills(memberID);
-  }
-
   // display details based on what the mouse is hovering over
   let detailHandler = (event) => {
     //know when to subtring up to
@@ -131,6 +77,107 @@ function App() {
     setDetails(detailRow)
   }
 
+  const findMemberID = (name, chamber) => {
+
+    const url = `https://api.propublica.org/congress/v1/116/${chamber}/members.json`
+    fetch(url,{
+      headers: {
+        "X-API-Key": apiKey2
+      }
+    })
+    .then(response => {return response.json()})
+    .then(searchResults => {
+      let membersArray = searchResults.results[0].members;
+      for(var i = 0; i < membersArray.length; i++) {
+        let person = membersArray[i];
+        let fullName = person.first_name + " " + person.last_name;
+        if(fullName === name) {
+          setMemberID(person.id);
+          break;
+        }
+      }
+    })
+    .catch(err => console.log(err))
+
+  }
+
+  //find a bill from the congressperson's memberID
+  const findBills = (memberID) => {
+    let type = "cosponsored"
+    //let memberID =  // member id for Abdnor James (Republican - South Dakota)ssssss
+    const url = `https://api.propublica.org/congress/v1/members/${memberID}/bills/${type}.json`;
+    fetch(url,{
+      headers: {
+        "X-API-Key": apiKey2
+      }
+    })
+    .then(response => {return response.json()})
+    .then(searchResults => {
+      console.log(searchResults)
+      let results = searchResults.results[0].bills;
+      //array of names of officials in those offices
+      // let officials = searchResults.officials;
+      var billsArray = [];
+
+      if (results == undefined) {
+        billsArray.push("No bills to report.")
+      } else {
+        billsArray.push(<h1>{searchResults.results[0].name}</h1>)
+        for(var i = 0; i < results.length; i++) {
+          const billRow = <div key={i}>
+            <h3> Bill - {results[i].short_title}: </h3>
+            <h6> Latest action date: {results[i].latest_major_action_date} </h6>
+            <h6>  Primary Subject: {results[i].primary_subject} </h6>
+            <h6>  Still active?: {results[i].active} </h6>
+            <h6>  Brief: {results[i].summary} </h6>
+            
+          </div>
+          billsArray.push(billRow);
+        }
+      }
+      //setting reps to list of all representatives' repRows
+      setBills(billsArray);
+
+    })
+    .catch(err => console.log(err))
+  }
+
+  //set the state of address
+  let changeAddress = (event) => {
+    setAddress(event.target.value);
+  }
+
+  //set the state of memberID
+  let changeID = (event) => {
+    setMemberID(event.target.value);
+  }
+
+  let changeSenator = (event) => {
+    setName(event.target.value);
+    setChamber("senate")
+  }
+
+  let changeRepresentative = (event) => {
+    setName(event.target.value);
+    setChamber("house")
+  }
+
+  //find the representatives based on the value in the address once the button is pressed
+  let addressChangeHandler = () => {
+    findReps(address);
+  }
+
+  //find bills based on member ID once the button is pressed
+  let billHandler = useEffect(() => {
+    // john lewis : L000287
+    findMemberID(name, chamber)
+    findBills(memberID);
+  })
+
+  // let billHandler = () => {
+  //   findMemberID(name, chamber)
+  //   findBills(memberID);
+  // }
 
   return (
     <div className="App">
@@ -151,6 +198,14 @@ function App() {
         <br></br>
         {details}
       </div>
+      <input 
+        id = 'senator'
+        onChange={changeSenator}
+        placeholder="Enter your senator to get their bills"/>
+      <input 
+        id = 'representative'
+        onChange={changeRepresentative}
+        placeholder="Enter your representative to get their bills"/>
       <input
         id='bills'
         onChange={changeID}
